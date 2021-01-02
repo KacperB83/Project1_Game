464c5e55c6e1734783db31000cc930ca067aed86
Kacper Bojarski <k.bojarski.pl@gmail.com>
2021-01-02 18:51:24 +0100

    2.01 commit

--- src/main/java/com/myProject/game/TicTacToe4.java
+++ src/main/java/com/myProject/game/TicTacToe4.java
@@ -8,7 +8,6 @@ import javafx.geometry.Pos;
 
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.input.MouseButton;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
@@ -23,10 +22,9 @@ import javafx.util.Duration;
 import java.util.ArrayList;
 import java.util.List;
 
-public class TicTacToe extends Application {
+public class TicTacToe4 extends Application {
 
-    /*
-       /* function alphaBeta(node, alpha, beta, maximisingPlayer) {
+   /* function alphaBeta(node, alpha, beta, maximisingPlayer) {
     var bestValue;
     if (node.children.length === 0) {
         bestValue = node.data;
@@ -63,22 +61,16 @@ public class TicTacToe extends Application {
     private Tile[][] board = new Tile[5][5];
     private List<Combo> combos = new ArrayList<>();
     private Pane root = new Pane();
-    private Button button;
 
     private Parent createContent() {
 
-        root.setPrefSize(900, 700);
-        button = new Button();
-        button.setText("Restart");
-        button.setAlignment(Pos.TOP_CENTER);
-       // button.setOnAction();
-        root.getChildren().add(button);
+        root.setPrefSize(750, 750);
 
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 5; j++) {
                 Tile tile = new Tile();
-                tile.setTranslateX(j * 100);
-                tile.setTranslateY(i* 100);
+                tile.setTranslateX(j * 150);
+                tile.setTranslateY(i* 150);
 
                 root.getChildren().add(tile);
 
@@ -86,26 +78,35 @@ public class TicTacToe extends Application {
             }
         }
         //poziom
-        for (int y = 0; y < 5; y++) {
-            combos.add(new Combo(board[0][y], board[1][y], board[2][y], board[3][y], board[4][y]));
+        for (int y = 0; y < 4; y++) {
+            combos.add(new Combo(board[0][y+1], board[1][y+1], board[2][y+1], board[3][y+1], board[4][y+1]));
+            combos.add(new Combo(board[0][y], board[1][y], board[2][y], board[3][y]));
         }
         //pion
-        for (int x = 0; x < 5; x++) {
-            combos.add(new Combo(board[x][0], board[x][1], board[x][2], board[x][3], board[x][4]));
+        for (int x = 0; x < 4; x++) {
+            combos.add(new Combo(board[x+1][0], board[x+1][1], board[x+1][2], board[x+1][3], board[x+1][4]));
+            combos.add(new Combo(board[x][0], board[x][1], board[x][2], board[x][3]));
         }
         //przekątne
-        combos.add(new Combo(board[0][0], board[1][1], board[2][2], board[3][3], board[4][4]));
-        combos.add(new Combo(board[0][0], board[1][1], board[2][2], board[3][3], board[4][4]));
+        combos.add(new Combo(board[1][1], board[2][2], board[3][3], board[4][4]));
+        combos.add(new Combo(board[0][0], board[1][1], board[2][2], board[3][3]));
+
+        combos.add(new Combo(board[3][1], board[2][2], board[1][3], board[0][4]));
+        combos.add(new Combo(board[4][0], board[3][1], board[2][2], board[1][3]));
+
+        combos.add(new Combo(board[3][0], board[2][1], board[1][2], board[0][3]));
+        combos.add(new Combo(board[4][1], board[3][2], board[2][3], board[1][4]));
+
+        combos.add(new Combo(board[0][1], board[1][2], board[2][3], board[3][4]));
+        combos.add(new Combo(board[1][0], board[2][1], board[3][2], board[4][3]));
+
 
         return root;
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        primaryStage.setTitle("TicTacToe");
-
         primaryStage.setScene(new Scene(createContent()));
-
         primaryStage.show();
     }
 
@@ -128,7 +129,7 @@ public class TicTacToe extends Application {
         root.getChildren().add(line);
 
         Timeline timeline = new Timeline();
-        timeline.getKeyFrames().add(new KeyFrame(Duration.seconds(2),
+        timeline.getKeyFrames().add(new KeyFrame(Duration.seconds(1),
                 new KeyValue(line.endXProperty(), combo.tiles[4].getCenterX()),
                 new KeyValue(line.endYProperty(), combo.tiles[4].getCenterY())));
         timeline.play();
@@ -136,18 +137,22 @@ public class TicTacToe extends Application {
 
     private class Combo {
         private Tile[] tiles;
-        public Combo (Tile... tiles) {
+
+        public Combo(Tile... tiles) {
             this.tiles = tiles;
         }
 
         public boolean isComplete() {
-            if (tiles[0].getValue().isEmpty() || tiles[4].getValue().isEmpty())
+            while (tiles[0].getValue().isEmpty() || tiles[4].getValue().isEmpty())
                 return false;
 
-            return tiles[0].getValue().equals(tiles[1].getValue())
-                    &&tiles[0].getValue().equals(tiles[2].getValue())
-                    &&tiles[0].getValue().equals(tiles[3].getValue())
-                    &&tiles[0].getValue().equals(tiles[4].getValue());
+            return
+                    tiles[0].getValue().equals(tiles[2].getValue())
+                            && tiles[0].getValue().equals(tiles[3].getValue())
+                            && tiles[0].getValue().equals(tiles[4].getValue()) ||
+                            tiles[0].getValue().equals(tiles[1].getValue())
+                                    && tiles[0].getValue().equals(tiles[2].getValue())
+                                    && tiles[0].getValue().equals(tiles[3].getValue());
         }
     }
 
@@ -155,11 +160,11 @@ public class TicTacToe extends Application {
         private Text text = new Text();
 
         public Tile() {
-            Rectangle border = new Rectangle(100, 100);
+            Rectangle border = new Rectangle(150, 150);
             border.setFill(null);
             border.setStroke(Color.BLACK);
 
-            text.setFont(Font.font(36));
+            text.setFont(Font.font(46));
 
             setAlignment(Pos.CENTER);
             getChildren().addAll(border, text);
@@ -182,10 +187,10 @@ public class TicTacToe extends Application {
             });
         }
         public double getCenterX() {
-            return getTranslateX() + 50;
+            return getTranslateX() + 75;
         }
         public double getCenterY() {
-            return getTranslateY() + 50;
+            return getTranslateY() + 75;
         }
 
         public String getValue() {
